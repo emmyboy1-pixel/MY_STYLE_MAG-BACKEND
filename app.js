a2821a69451acd5aac044895b38468b78873680e
emmyboy1-pixel <www.emmyboy1@gmail.com>
2025-06-03 21:20:45 +0100

    commit message

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
+import upload from './middleware/multer.js';
+import cloudinary from './config/cloudinary.js';
+import fs from 'fs';
 import { sequelize } from './config/dbConfig.js';
-import userRoutes from './routes/userRoutes.js'
-import outfitRoutes from './routes/outfitRoutes.js'
-import lookbookRoutes from './routes/lookbookRoutes.js'
-import categoryRoutes from './routes/categoryRoutes.js'
+import userRoutes from './routes/userRoutes.js';
+import outfitRoutes from './routes/outfitRoutes.js';
+import lookbookRoutes from './routes/lookbookRoutes.js';
+import categoryRoutes from './routes/categoryRoutes.js';
 
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.post('/uploads', upload.single('image'), async (req, res) => {
+  try {
+    const filePath = req.file.path;
+
+    // Upload image to Cloudinary
+    const result = await cloudinary.uploader.upload(filePath, {
+      folder: 'fashionline',
+    });
+
+    //this will Delete the file after it has been uploaded to Cloudinary
+    fs.unlinkSync(filePath);
+
+    res.json({
+      message: 'Upload successful',
+      url: result.secure_url,
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Upload failed', details: error.message });
+  }
+});
 
 // middlewares here
 app.use(express.json());
