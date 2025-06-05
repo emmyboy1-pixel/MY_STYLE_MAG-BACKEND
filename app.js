d3c4e795b47801ae6bed452322dda2902ad8aa15
emmyboy1-pixel <www.emmyboy1@gmail.com>
2025-06-05 02:09:09 +0100

    import multer middleware into app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cloudinary from './config/cloudinary.js';import fs from 'fs';
+import cloudinary from './config/cloudinary.js';
+import upload from './config/multer.js';
 import { sequelize } from './config/dbConfig.js';
 import userRoutes from './routes/userRoutes.js';
 import outfitRoutes from './routes/outfitRoutes.js';
