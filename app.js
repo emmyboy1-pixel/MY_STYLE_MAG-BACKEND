8fdd9e2a7f0b56b627a702316650691d70f17e7d
emmyboy1-pixel <www.emmyboy1@gmail.com>
2025-06-05 01:53:28 +0100

    commit message

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ import { authenticateUser } from "./middleware/authentication.js";
 =======
 import express from 'express';
 import dotenv from 'dotenv';
-import upload from './middleware/multer.js';
-import cloudinary from './config/cloudinary.js';
-import fs from 'fs';
+import cloudinary from './config/cloudinary.js';import fs from 'fs';
 import { sequelize } from './config/dbConfig.js';
 import userRoutes from './routes/userRoutes.js';
 import outfitRoutes from './routes/outfitRoutes.js';
@@ -28,42 +26,42 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.post('/uploads', upload.single('image'), async (req, res) => {
-  try {
-    const filePath = req.file.path;
-
-    // Upload image to Cloudinary
-    const result = await cloudinary.uploader.upload(filePath, {
-      folder: 'fashionline',
-    });
-
-    //this will Delete the file after it has been uploaded to Cloudinary
-    fs.unlinkSync(filePath);
-
-    res.json({
-      message: 'Upload successful',
-      url: result.secure_url,
-    });
-  } catch (error) {
-    res.status(500).json({ error: 'Upload failed', details: error.message });
-  }
+app.get("/", (req, res) => {
+    res.send("Welcome to the Fashion App API");
 });
-
 // middlewares here
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
+<<<<<<< HEAD
 app.get("/api/v1/auth", authenticateUser, (req, res) => {
   res.status(200).send("Authentication works perfectly");
 });
 
+=======
+>>>>>>> c6b16e1 (commit message)
 // routes here
 app.use("/api/v1", authRouter);
 app.use("/api/v1/categories", categoryRoutes);
 app.use("/api/users", userRoutes);
+<<<<<<< HEAD
 app.use("/api/lookbooks", lookbookRoutes);
 app.use("/api/v1/outfits", outfitRoutes);
 
+=======
+app.use("/api/lookbooks", lookbookRoutes );
+app.use("/api/outfits", outfitRoutes);
+app.use("/api/upload", uploadRoutes);
+
+app.post('/api/upload', upload.single('image'), (req, res) => {
+  cloudinary.uploader.upload(req.file.path, (error, result) => {
+    if (error) {
+      return res.status(500).json({ error: 'Failed to upload image' });
+    }
+    res.status(200).json({ url: result.secure_url });
+  })
+});
+>>>>>>> c6b16e1 (commit message)
 // syncing databse and running port number
 sequelize
   .sync({ alter: true })
