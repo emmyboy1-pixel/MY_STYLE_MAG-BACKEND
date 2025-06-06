3b29cad5e2b76d12bb814acbbf4b10d2a1d8d2cb
Valerie-Anne Morveh <morevimgh14@gmail.com>
2025-06-06 19:52:33 +0000

    Added lookbook routes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,8 +3,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// setting up our database connection
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT
-});
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
+
+const options = {
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'mysql',
+  logging: false
+};
+
+console.log('Sequelize options:', options);
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  options
+);
