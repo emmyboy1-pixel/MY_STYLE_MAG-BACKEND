01c26daef93126f7ca670704390623bd01f17918
okistaylor <72969270+TaylorOkis@users.noreply.github.com>
2025-06-12 14:28:34 +0100

    Add try/catch to registration

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -12,38 +12,45 @@ const generateToken = () => {
 };
 
 const register = async (req, res) => {
-  const { name, email, password, role } = req.body;
+  try {
+    const { name, email, password, role } = req.body;
 
-  const existingEmail = await User.findOne({ where: { email: email } });
+    const existingEmail = await User.findOne({ where: { email: email } });
 
-  if (existingEmail) {
-    res
-      .status(404)
-      .json({ status: false, message: "Email already in use", data: [] });
-  }
+    if (existingEmail) {
+      res
+        .status(404)
+        .json({ status: false, message: "Email already in use", data: [] });
+    }
 
-  const hashed_password = bcrypt.hashSync(password, 10);
+    const hashed_password = bcrypt.hashSync(password, 10);
 
-  const user = await User.create({
-    name,
-    email,
-    password: hashed_password,
-    role: "user" || role,
-  });
+    const user = await User.create({
+      name,
+      email,
+      password: hashed_password,
+      role: "user" || role,
+    });
 
-  if (!user) {
-    res.status(500).json({
-      status: false,
-      message: "Registration error",
+    if (!user) {
+      res.status(500).json({
+        status: false,
+        message: "Registration error",
+        data: [],
+      });
+    }
+
+    res.status(201).json({
+      status: true,
+      message: "User created successfully",
       data: [],
     });
+  } catch (error) {
+    res.status(500).json({
+      error: "User Registration unsuccessfully",
+      details: error.message,
+    });
   }
-
-  res.status(201).json({
-    status: true,
-    message: "User created successfully",
-    data: [],
-  });
 };
 
 const login = async (req, res) => {
